6fdeee40dbbc58cddcf2bbcbb209bf2d33bc4668
Jim McCallum <jlmccallum2@gmail.com>
2025-02-04 10:24:33 +0800

    Geo model no fault working

--- scripts/process_structuralmodel.py
+++ scripts/process_structuralmodel.py
@@ -35,7 +35,7 @@ def prepare_strat_column(structuralmodel):
             mx = np.inf
         else:
             mx = vals[i-1]
-        if i == (len(sequences) - 1) or sequences[i] != sequences[i+1]:
+        if i != (len(sequences) - 1) and sequences[i] != sequences[i+1]:
             mn = -np.inf
         else:
             mn = vals[i]
@@ -49,10 +49,16 @@ def prepare_strat_column(structuralmodel):
     structuralmodel.cmap = cmap
     structuralmodel.norm = norm
     
-def prepare_geodata(structuralmodel, Lleyland = False, Brett = True, Petroleum = True, Model = 1):
+def prepare_geodata(structuralmodel, Lleyland = False, Brett = True, Petroleum = True, Model = 1,extent = None ):
 
-    x0, y0, z0 = structuralmodel.x0, structuralmodel.y0, structuralmodel.z0
-    x1, y1, z1 = structuralmodel.x1, structuralmodel.y1, structuralmodel.z1
+    if type(extent) == type(None):
+        x0, y0, z0 = structuralmodel.x0, structuralmodel.y0, structuralmodel.z0
+        x1, y1, z1 = structuralmodel.x1, structuralmodel.y1, structuralmodel.z1
+    else:
+        x0, y0, z0 = extent[0], extent[1], extent[2]
+        x1, y1, z1 = extent[3], extent[4], extent[5]      
+
+    
     strat = structuralmodel.strat
     
     bore_info = pd.read_excel(structuralmodel.geodata_fname, sheet_name=structuralmodel.data_sheetname)
@@ -84,13 +90,14 @@ def prepare_geodata(structuralmodel, Lleyland = False, Brett = True, Petroleum =
 
         EOH = max(stuff)
         #print(EOH)
-
+        EOH_flag = False
         boreid = data_list[i][3]
         easting, northing = data_list[i][0], data_list[i][1]
         groundlevel = data_list[i][4]
+        bottom = np.copy(groundlevel)
         # First channp.nan, np.nan, np.nange - we can get the norms from the geophys data...
         gx, gy, gz = 0.0, 0.0, 1.0  # np.nan, np.nan,np.nan
-
+        
         # Add data for groundlevel
         val = strat.val[0]
         formatted_data.append(
@@ -121,6 +128,7 @@ def prepare_geodata(structuralmodel, Lleyland = False, Brett = True, Petroleum =
             )
             current_bottom = np.copy(bottom)
         #Kings park
+        TKP = np.copy(bottom)
         if isinstance(data_list[i][8], numbers.Number) == True:
             bottom = groundlevel - float(data_list[i][8])  # Ground surface - kinhs park base (mbgl)
             val = strat.val[2]  # designated isovalue
@@ -130,17 +138,34 @@ def prepare_geodata(structuralmodel, Lleyland = False, Brett = True, Petroleum =
                 [boreid, easting, northing, bottom, val, lithid, feat_name, gx, gy, gz,"raw_data"]
             )
             current_bottom = np.copy(bottom)  
+        if TKP != bottom: #add top of kings park formation:
+            val = strat.val[1]
+            lithid = strat.lithid[2]  # lithology id
+            feat_name = strat.sequence[2]  # sequence name
+            formatted_data.append(
+                [boreid, easting, northing, TKP, val, lithid, feat_name, gx, gy, gz,"raw_data"]
+            )     
+        else: #hide it    
+            val = strat.val[1]
+            lithid = strat.lithid[2]  # lithology id
+            feat_name = strat.sequence[2]  # sequence name
+            formatted_data.append(
+                [boreid, easting, northing, bottom, val, lithid, feat_name, gx, gy, gz,"raw_data"]
+            )     
         #Tertiaty_unconformity
-        bottom = np.copy(current_bottom)  # current depth
-        val = 0  # designated isovalue
-        lithid = 'base_tert'  # lithology id
-        feat_name = "Tert_U"  # sequence name
-        formatted_data.append(
-            [boreid, easting, northing, bottom, val, lithid, feat_name, 0, 0, 1,"raw_data"]
-        )
+        gx, gy, gz = np.nan, np.nan,np.nan
+        if not EOH_flag:
+            bottom = np.copy(current_bottom)  # current depth
+            val = 0  # designated isovalue
+            lithid = 'base_tert'  # lithology id
+            feat_name = "Tert_U"  # sequence name
+            formatted_data.append(
+                [boreid, easting, northing, bottom, val, lithid, feat_name, 0, 0, 1,"raw_data"]
+            )
         #Lancelin formation
         ldum = [3,4,5]
         cdum = [11,12,13]
+        TLF = np.copy(bottom)
         for j in range(3):
             if isinstance(data_list[i][cdum[j]], numbers.Number) == True:
                 bottom = groundlevel - float(data_list[i][cdum[j]])  # Ground surface - kinhs park base (mbgl)
@@ -150,17 +175,34 @@ def prepare_geodata(structuralmodel, Lleyland = False, Brett = True, Petroleum =
                 formatted_data.append(
                     [boreid, easting, northing, bottom, val, lithid, feat_name, gx, gy, gz,"raw_data"]
                 )
-                current_bottom = np.copy(bottom)              
+                current_bottom = np.copy(bottom)     
+        
+        if bottom == TLF: #hide it
+            val = strat.val[5]
+            lithid = strat.lithid[5]  # lithology id
+            feat_name = strat.sequence[5]  # sequence name
+            formatted_data.append(
+                [boreid, easting, northing, bottom, val, lithid, feat_name, gx, gy, gz,"raw_data"]
+            )                      
+        elif isinstance(data_list[i][11], numbers.Number) == True:    
+            val = strat.val[2]
+            lithid = strat.lithid[3]  # lithology id
+            feat_name = strat.sequence[3]  # sequence name
+            formatted_data.append(
+                [boreid, easting, northing, TLF, val, lithid, feat_name, gx, gy, gz,"raw_data"]
+            )    
 
         #Lancelin _ unconformity
         bottom = np.copy(current_bottom)  # current depth
-        val = 0  # designated isovalue
-        lithid = 'base_lanc'  # lithology id
-        feat_name = "Lanc_U"  # sequence name
-        formatted_data.append(
-            [boreid, easting, northing, bottom, val, lithid, feat_name, 0, 0, 1,"raw_data"]
-        )
-
+        if not EOH_flag:
+            val = 0  # designated isovalue
+            lithid = 'base_lanc'  # lithology id
+            feat_name = "Lanc_U"  # sequence name
+            formatted_data.append(
+                [boreid, easting, northing, bottom, val, lithid, feat_name, 0, 0, 1,"raw_data"]
+            )
+            
+        TOF = np.copy(bottom)
         #Osbourne_formation
         ldum = [6,7,8]
         cdum = [14,15,17]
@@ -174,15 +216,32 @@ def prepare_geodata(structuralmodel, Lleyland = False, Brett = True, Petroleum =
                     [boreid, easting, northing, bottom, val, lithid, feat_name, gx, gy, gz,"raw_data"]
                 )
                 current_bottom = np.copy(bottom)     
-
+        if TOF == bottom:
+            val = strat.val[8]
+            lithid = strat.lithid[8]  # lithology id
+            feat_name = strat.sequence[8]  # sequence name
+            formatted_data.append(
+                [boreid, easting, northing, bottom, val, lithid, feat_name, gx, gy, gz,"raw_data"]
+            )                      
+        elif isinstance(data_list[i][14], numbers.Number) == True:    
+            val = strat.val[5]
+            lithid = strat.lithid[6]  # lithology id
+            feat_name = strat.sequence[6]  # sequence name
+            formatted_data.append(
+                [boreid, easting, northing, TOF, val, lithid, feat_name, gx, gy, gz,"raw_data"]
+            ) 
+            
         #Osbourne _ unconformity
         bottom = np.copy(current_bottom)  # current depth
-        val = 0  # designated isovalue
-        lithid = 'base_Osb'  # lithology id
-        feat_name = "Osb_U"  # sequence name
-        formatted_data.append(
-            [boreid, easting, northing, bottom, val, lithid, feat_name, 0, 0, 1,"raw_data"]
-        )
+        TOL = np.copy(bottom)
+        
+        if not EOH_flag:
+            val = 0  # designated isovalue
+            lithid = 'base_Osb'  # lithology id
+            feat_name = "Osb_U"  # sequence name
+            formatted_data.append(
+                [boreid, easting, northing, bottom, val, lithid, feat_name, 0, 0, 1,"raw_data"]
+            )
 
         #Leedervilles
         ldum = [9,10,11]
@@ -197,16 +256,34 @@ def prepare_geodata(structuralmodel, Lleyland = False, Brett = True, Petroleum =
                     formatted_data.append(
                         [boreid, easting, northing, bottom, val, lithid, feat_name, gx, gy, gz,"raw_data"]
                     )
-                    current_bottom = np.copy(bottom)         
+                    current_bottom = np.copy(bottom)   
+                else:
+                    EOH_flag = True      
         #Leederville _ unconformity
+        if TOL == bottom and not EOH_flag:
+            val = strat.val[11]
+            lithid = strat.lithid[11]  # lithology id
+            feat_name = strat.sequence[11]  # sequence name
+            formatted_data.append(
+                [boreid, easting, northing, bottom, val, lithid, feat_name, gx, gy, gz,"raw_data"]
+            )                      
+        elif isinstance(data_list[i][18], numbers.Number) == True:    
+            val = strat.val[8]
+            lithid = strat.lithid[9]  # lithology id
+            feat_name = strat.sequence[9]  # sequence name
+            formatted_data.append(
+                [boreid, easting, northing, TOL, val, lithid, feat_name, gx, gy, gz,"raw_data"]
+            ) 
         bottom = np.copy(current_bottom)  # current depth
-        val = 0  # designated isovalue
-        lithid = 'base_Leed'  # lithology id
-        feat_name = "Leed_U"  # sequence name
-        formatted_data.append(
-            [boreid, easting, northing, bottom, val, lithid, feat_name, 0, 0, 1,"raw_data"]
-        )
+        if not EOH_flag:
+            val = 0  # designated isovalue
+            lithid = 'base_Leed'  # lithology id
+            feat_name = "Leed_U"  # sequence name
+            formatted_data.append(
+                [boreid, easting, northing, bottom, val, lithid, feat_name, 0, 0, 1,"raw_data"]
+            )
 
+        TOW = np.copy(bottom)
         #SPS and Gage
         ldum = [12,13]
         cdum = [21,22]
@@ -220,19 +297,37 @@ def prepare_geodata(structuralmodel, Lleyland = False, Brett = True, Petroleum =
                     formatted_data.append(
                         [boreid, easting, northing, bottom, val, lithid, feat_name, gx, gy, gz,"raw_data"]
                     )
-                    current_bottom = np.copy(bottom)          
+                else:
+                    EOH_flag = True   
+                current_bottom = np.copy(bottom) 
+                     
         #Warnbro _ unconformity
+        if TOW == bottom and not EOH_flag:
+            val = strat.val[13]
+            lithid = strat.lithid[13]  # lithology id
+            feat_name = strat.sequence[13]  # sequence name
+            formatted_data.append(
+                [boreid, easting, northing, bottom, val, lithid, feat_name, gx, gy, gz,"raw_data"]
+            )                      
+        elif isinstance(data_list[i][21], numbers.Number) == True:    
+            val = strat.val[11]
+            lithid = strat.lithid[12]  # lithology id
+            feat_name = strat.sequence[12]  # sequence name
+            formatted_data.append(
+                [boreid, easting, northing, TOW, val, lithid, feat_name, gx, gy, gz,"raw_data"]
+            ) 
         bottom = np.copy(current_bottom)  # current depth
-        val = 0  # designated isovalue
-        lithid = 'base_Warn'  # lithology id
-        feat_name = "Warn_U"  # sequence name
-        formatted_data.append(
-            [boreid, easting, northing, bottom, val, lithid, feat_name, 0, 0, 1,"raw_data"]
-        )
-
+        if not EOH_flag:
+            val = 0  # designated isovalue
+            lithid = 'base_Warn'  # lithology id
+            feat_name = "Warn_U"  # sequence name
+            formatted_data.append(
+                [boreid, easting, northing, bottom, val, lithid, feat_name, 0, 0, 1,"raw_data"]
+            )
+        TOY = np.copy(bottom)
         #Yaragadee, or parmelias, no real yaragadee bases
-        ldum = [14,15,16]
-        cdum = [23,24,25]
+        ldum = [14,15,16,17]
+        cdum = [23,24,25,26]
         for j in range(3):
             if isinstance(data_list[i][cdum[j]], numbers.Number) == True:
                 if float(data_list[i][cdum[j]]) < EOH:
@@ -244,7 +339,14 @@ def prepare_geodata(structuralmodel, Lleyland = False, Brett = True, Petroleum =
                         [boreid, easting, northing, bottom, val, lithid, feat_name, np.nan, np.nan, np.nan,"raw_data"]
                     )
                     current_bottom = np.copy(bottom)
-
+        if isinstance(data_list[i][23], numbers.Number) == True:    
+            val = strat.val[13]
+            lithid = strat.lithid[14]  # lithology id
+            feat_name = strat.sequence[14]  # sequence name
+            print('here', boreid,lithid)
+            formatted_data.append(
+                [boreid, easting, northing, TOY, val, lithid, feat_name, gx, gy, gz,"raw_data"]
+            ) 
     data = pd.DataFrame(formatted_data)
     data.columns = [
         "ID",
